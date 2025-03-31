db80cdf8a9106f6fb4cd66f3e35e8e241ecf3ef5
sauerbraten <willing.alexander@googlemail.com>
2025-03-31 16:54:21 +0200

    reduce proxy delay to 1s

--- proxy/root/sauer-proxy.sh
+++ proxy/root/sauer-proxy.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 server_num=$1
 source .env # sets SERVER_IP
 
-exec ./sauer-proxy --listen-port ${server_num}000 --remote-port ${server_num}000 --register-master --forward-ips --delay 5000 ${SERVER_IP}
+exec ./sauer-proxy --listen-port ${server_num}000 --remote-port ${server_num}000 --register-master --forward-ips --delay 1000 ${SERVER_IP}
